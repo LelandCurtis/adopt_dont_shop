4e1d39c9b94bb6f08cd1337bbe602b935ebe0247
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-13 16:32:43 -0700

    Move admin controllers and views within folders

--- config/routes.rb
+++ config/routes.rb
@@ -46,5 +46,5 @@ Rails.application.routes.draw do
   get '/veterinary_offices/:veterinary_office_id/veterinarians/new', to: 'veterinarians#new'
   post '/veterinary_offices/:veterinary_office_id/veterinarians', to: 'veterinarians#create'
 
-  get '/admin/shelters', to: 'admin_shelters#index'
+  get '/admin/shelters', to: 'admin/shelters#index'
 end
