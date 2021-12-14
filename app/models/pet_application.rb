ed815f5fb869fe274f010868335e63108ef08942
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-14 16:02:43 -0700

    switch : symbols and fix typos.

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -2,10 +2,10 @@ class PetApplication <ApplicationRecord
   belongs_to :pet
   belongs_to :application
 
-  def already_approved
+  def already_approved?
     approved_pet_apps = PetApplication.all
                                       .join(:applications)
-                                      .where(:applications{status: "Approved"}, :pet_applications{pet_id: pet_id})
+                                      .where(applications: {status: "Approved"}, pet_applications: {pet_id: pet_id})
     if approved_pet_apps.count > 0
       true
     else
