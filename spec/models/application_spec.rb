973bf39bf14bbccd086e32b76b297f1cb09e98ca
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-14 14:38:55 -0700

    Write update_status tests.

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -72,4 +72,56 @@ RSpec.describe Application, type: :model do
       expect(@pet_1.adoptable).to eq(true)
     end
   end
+
+  describe '.update_status' do
+    it "does nothing if the application status matches the pet_application status requirement" do
+      expect(@application_1.check_status).to eq("Pending")
+      @application_1.update_status
+      expect(@application_1.check_status).to eq("Pending")
+    end
+
+    it "if pet_app status is different from app status, and it should be approved, approve application and update pets" do
+      application = Application.create(name: 'Steve', address: '135 Waddle Road', city: 'Dallas', state: 'TX', zip: 75001, description: "I really want a dog", status: "Pending")
+
+      pet_application_1 = PetApplication.create!(pet_id: @pet_1.id, application_id: application_1.id, status: "Pending")
+      pet_application_2 = PetApplication.create!(pet_id: @pet_2.id, application_id: application_1.id, status: "Approved")
+      pet_application_3 = PetApplication.create!(pet_id: @pet_3.id, application_id: application_1.id, status: "Approved")
+
+      expect(application.check_status).to eq("Pending")
+      expect(application.status).to eq("Pending")
+      pet_application_1.update(status: 'Approved')
+      expect(application.check_status).to eq("Approved")
+      expect(application.status).to eq("Pending")
+
+      application.update_status
+
+      expect(application.status).to eq("Approved")
+      expect(@pet_1.adoptable).to eq(false)
+      expect(@pet_2.adoptable).to eq(false)
+      expect(@pet_3.adoptable).to eq(false)
+    end
+
+    it "if pet_app status is different from app status, and it should be rejected, reject application and don't update pets" do
+      application = Application.create(name: 'Steve', address: '135 Waddle Road', city: 'Dallas', state: 'TX', zip: 75001, description: "I really want a dog", status: "Pending")
+
+      pet_application_1 = PetApplication.create!(pet_id: @pet_1.id, application_id: application_1.id, status: "Pending")
+      pet_application_2 = PetApplication.create!(pet_id: @pet_2.id, application_id: application_1.id, status: "Approved")
+      pet_application_3 = PetApplication.create!(pet_id: @pet_3.id, application_id: application_1.id, status: "Approved")
+
+      expect(application.check_status).to eq("Pending")
+      expect(application.status).to eq("Pending")
+      pet_application_1.update(status: 'Rejected')
+      expect(application.check_status).to eq("Rejected")
+      expect(application.status).to eq("Pending")
+
+      application.update_status
+
+      expect(application.status).to eq("Rejected")
+      expect(@pet_1.adoptable).to eq(true)
+      expect(@pet_2.adoptable).to eq(true)
+      expect(@pet_3.adoptable).to eq(true)
+    end
+
+
+  end
 end
