a8901e6398beb61355bc70d62eecf011bfe0f649
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-13 17:26:43 -0700

    Have admin app controller deliver all pet_applications.

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,7 +1,6 @@
 class Admin::ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
-
-    @pets = @application.pets
+    @pet_applications = @application.pet_applications
   end
 end
