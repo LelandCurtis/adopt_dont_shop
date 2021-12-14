262c9c9b6002ddb5d681b44277e89c128b16384c
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-14 15:59:30 -0700

    Write already approved model method in pet_applicaiton.

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -1,4 +1,15 @@
 class PetApplication <ApplicationRecord
   belongs_to :pet
   belongs_to :application
+
+  def already_approved
+    approved_pet_apps = PetApplication.all
+                                      .join(:applications)
+                                      .where(:applications{status: "Approved"}, :pet_applications{pet_id: pet_id})
+    if approved_pet_apps.count > 0
+      true
+    else
+      false
+    end
+  end
 end
