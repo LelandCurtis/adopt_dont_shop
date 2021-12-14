887638feed167ce79398efae5072ce64ec6816f9
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-14 16:06:03 -0700

    remove .all. change applications to application.

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -3,8 +3,7 @@ class PetApplication <ApplicationRecord
   belongs_to :application
 
   def already_approved?
-    approved_pet_apps = PetApplication.all
-                                      .join(:applications)
+    approved_pet_apps = PetApplication.joins(:application)
                                       .where(applications: {status: "Approved"}, pet_applications: {pet_id: pet_id})
     if approved_pet_apps.count > 0
       true
