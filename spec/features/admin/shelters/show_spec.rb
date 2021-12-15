d30b2ede11e9a8c6f3b8623895fd2335c5aac0cf
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-14 20:37:09 -0700

    Update test files. Add action required pending pet list feature test.

--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -8,6 +8,15 @@ RSpec.describe 'admin shelters show page' do
     @shelter_1.pets.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
     @shelter_1.pets.create!(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
     @shelter_3.pets.create!(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+
+    @pet_1 = Pet.create!(adoptable: true, age: 10, breed: "Hound", name: "Pete", shelter_id: @shelter_1.id)
+    @pet_2 = Pet.create!(adoptable: true, age: 3, breed: "GSP", name: "Newton", shelter_id: @shelter_2.id)
+    @pet_3 = Pet.create!(adoptable: true, age: 13, breed: "German Shepard", name: "Molly", shelter_id: @shelter_3.id)
+    @pet_4 = Pet.create!(adoptable: true, age: 13, breed: "German Shepard", name: "Molly", shelter_id: @shelter_1.id)
+    @application_1 = Application.create!(name: 'Steve', address: '135 Waddle Road', city: 'Dallas', state: 'TX', zip: 75001, description: "I really want a dog", status: "Pending")
+    @pet_application_1 = PetApplication.create!(pet_id: @pet_1.id, application_id: @application_1.id, status: "Approved")
+    @pet_application_2 = PetApplication.create!(pet_id: @pet_2.id, application_id: @application_1.id, status: "Rejected")
+    @pet_application_4 = PetApplication.create!(pet_id: @pet_4.id, application_id: @application_1.id, status: "Pending")
   end
 
   it 'shows each shelter name and full address' do
@@ -38,6 +47,16 @@ RSpec.describe 'admin shelters show page' do
 
     within 'div.statistics' do
       expect(page).to have_content("#{@shelter_1.adopted_pet_count} pets have been adopted from this shelter")
+    end
+  end
+
+  it "in the action required section I see a list of all pets for this shelter that have a pending application and have not yet been marked approved or rejected" do
+    visit "/admin/shelters/#{@shelter_1.id}"
+
+    within 'div.action_required' do
+      expect(page).to have_content(@pet_4.name)
+      expect(page).to_not have_content(@pet_1.name)
+      expect(page).to_not have_content(@pet_2.name)
     end
   end
 end
