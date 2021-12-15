6df0f8e0e48587e23bf0fde547df1daa3f6c9de2
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-14 19:08:58 -0700

    Add tests for links to shelter show page.

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -8,6 +8,16 @@ RSpec.describe 'admin shelter index page' do
     @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
     @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
     @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+
+    @pet_1 = Pet.create!(adoptable: true, age: 10, breed: "Hound", name: "Pete", shelter_id: @shelter_1.id)
+    @pet_2 = Pet.create!(adoptable: true, age: 3, breed: "GSP", name: "Newton", shelter_id: @shelter_2.id)
+    @pet_3 = Pet.create!(adoptable: true, age: 13, breed: "German Shepard", name: "Molly", shelter_id: @shelter_3.id)
+    @pet_4 = Pet.create!(adoptable: true, age: 13, breed: "German Shepard", name: "Molly", shelter_id: @shelter_1.id)
+    @application_1 = Application.create!(name: 'Steve', address: '135 Waddle Road', city: 'Dallas', state: 'TX', zip: 75001, description: "I really want a dog", status: "Pending")
+    @pet_application_1 = PetApplication.create!(pet_id: @pet_1.id, application_id: @application_1.id)
+    @pet_application_2 = PetApplication.create!(pet_id: @pet_2.id, application_id: @application_1.id)
+    @pet_application_4 = PetApplication.create!(pet_id: @pet_4.id, application_id: @application_1.id)
+
   end
 
   it 'lists all the shelters in reverse alphabetical order when in admin mode' do
@@ -19,21 +29,25 @@ RSpec.describe 'admin shelter index page' do
   end
 
   it 'has a section that lists only shelters with pending applications and they are listed alphabetically' do
-
-    pet_1 = Pet.create!(adoptable: true, age: 10, breed: "Hound", name: "Pete", shelter_id: @shelter_1.id)
-    pet_2 = Pet.create!(adoptable: true, age: 3, breed: "GSP", name: "Newton", shelter_id: @shelter_2.id)
-    pet_3 = Pet.create!(adoptable: true, age: 13, breed: "German Shepard", name: "Molly", shelter_id: @shelter_3.id)
-    pet_4 = Pet.create!(adoptable: true, age: 13, breed: "German Shepard", name: "Molly", shelter_id: @shelter_1.id)
-    application_1 = Application.create!(name: 'Steve', address: '135 Waddle Road', city: 'Dallas', state: 'TX', zip: 75001, description: "I really want a dog", status: "Pending")
-    pet_application_1 = PetApplication.create!(pet_id: pet_1.id, application_id: application_1.id)
-    pet_application_2 = PetApplication.create!(pet_id: pet_2.id, application_id: application_1.id)
-    pet_application_4 = PetApplication.create!(pet_id: pet_4.id, application_id: application_1.id)
-
     visit '/admin/shelters'
     expect(page).to have_content("Shelters with Pending Applications")
 
     within('div.pending') do
       expect(@shelter_1.name).to appear_before(@shelter_2.name)
+    end
+  end
+
+  it 'each name is a link to the show page' do
+    visit '/admin/shelters'
+
+    within('div.shelters') do
+      click_link "#{@shelter_1.name}"
+      expect(current_path).to eq("/admin/shelters/#{@shelter_1.id}")
+    end
+
+    within('div.pending') do
+      click_link "#{@shelter_1.name}"
+      expect(current_path).to eq("/admin/shelters/#{@shelter_1.id}")
     end
   end
 end
