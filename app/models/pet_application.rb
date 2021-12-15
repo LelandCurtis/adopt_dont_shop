d79daadfa4284d12560d7e79a0dd5ad32ce73830
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-15 11:53:22 -0700

    Write pending model method for pet app.

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -5,4 +5,8 @@ class PetApplication <ApplicationRecord
   def already_approved?
     PetApplication.joins(:application).exists?(applications: {status: "Approved"}, pet_applications: {pet_id: pet_id})
   end
+
+  def self.pending
+    where(status: "Pending")
+  end
 end
