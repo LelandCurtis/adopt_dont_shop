9f05f164e7536d9d83cb9c8f995796188dd41e35
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-14 18:23:55 -0700

    Add controller, route and new show page.

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -3,4 +3,9 @@ class Admin::SheltersController < ApplicationController
     @shelters = Shelter.order_by_name_desc
     @pending_shelters = Shelter.with_pending_applications
   end
+
+  def show
+    @sql_shelter_name = Shelter.sql_name(params[:id])
+    @sql_shelter_address = Shelter.sql_address(params[:id])
+  end
 end
