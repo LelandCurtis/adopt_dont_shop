c2f04f6e3eea4324ef74f4139eb58614ea661594
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-15 16:14:00 -0700

    Update with zip validatations removed.

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,8 +3,9 @@ class Application < ApplicationRecord
   validates :address, presence: true
   validates :city, presence: true
   validates :state, presence: true
-  validates :zip, presence: true, numericality: true
-  validates_length_of :zip, is: 5
+  #validates :zip, presence: true
+  #validates :zip, numericality: true
+  #validates_length_of :zip, is: 5
   validates :status, presence: true
   has_many :pet_applications
   has_many :pets, through: :pet_applications
