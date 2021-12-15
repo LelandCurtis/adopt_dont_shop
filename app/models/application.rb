3bb9b4525ba5aace978d6cf211f26d548ce69245
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-15 14:54:28 -0700

    Add validations for applications.

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,11 @@
 class Application < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true, numericality: true
+  validates_length_of :zip, is: 5
+  validates :status, presence: true
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 
