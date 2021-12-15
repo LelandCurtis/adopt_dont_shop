40447d1033d1e6505218c9a54e37b93268031874
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-14 18:10:54 -0700

    refactor already_approved? to use exists?

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -2,13 +2,16 @@ class PetApplication <ApplicationRecord
   belongs_to :pet
   belongs_to :application
 
+  # def already_approved?
+  #   approved_pet_apps = PetApplication.joins(:application)
+  #                                     .where(applications: {status: "Approved"}, pet_applications: {pet_id: pet_id})
+  #   if approved_pet_apps.count > 0
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
   def already_approved?
-    approved_pet_apps = PetApplication.joins(:application)
-                                      .where(applications: {status: "Approved"}, pet_applications: {pet_id: pet_id})
-    if approved_pet_apps.count > 0
-      return true
-    else
-      return false
-    end
+    PetApplication.joins(:application).exists?(applications: {status: "Approved"}, pet_applications: {pet_id: pet_id})
   end
 end
